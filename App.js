3d191c8a78574521bcf509b36bc857e2a7c9f708
victor19pa <vpinedaa@unah.hn>
2022-02-16 08:10:37 -0600

    creacion

---
+++ App.js
@@ -0,0 +1,27 @@
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+ 
+  return (
+    <>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
